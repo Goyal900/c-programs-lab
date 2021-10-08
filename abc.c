bf295fbfc9f799e1a39e5ae1358bd8b48c25d5d1
Goyal900 <92144314+Goyal900@users.noreply.github.com>
2021-10-08 08:15:12 +0200

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float p,r,t;
+printf("value of p : ");
+scanf("%f",&p);
+printf("value of r : ");
+scanf("%f",&r);
+printf("value of t : ");
+scanf("%f",&t);
+printf("the simple interest is : %f\n",(p*r*t)/100);
+} 
