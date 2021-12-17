294f0da0d95996ce63c3c7d2f8bb19270c42015a
Goyal900 <92144314+Goyal900@users.noreply.github.com>
2021-12-17 12:53:54 +0530

    Add files via upload

--- ram.c
+++ ram.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
-int main()
-{
-int n, i,j;
-printf("enter the size of array\n");
+int main(){
+int n,temp;
+printf("enter the total no. of elements\n");
 scanf("%d",&n);
-int a[n];
-for(i=0;i<n;i++)
-{ scanf("%d",&a[i]);
+int a[n],i,j;
+for(i=0;i<n;i++){
+printf("enter the %d element",i);
+scanf("%d",&a[i]);
 }
-for(j=0;j<n;j++){
-printf("%d",a[j]);
+for(i=0,j=n-1;i<n/2;i++,j--)
+{ temp=a[i];
+a[i]=a[n-1-i];
+a[j]=temp;
 }
+for(i=0;i<n;i++){
+printf("%d  ",a[i]);}
 return 0;
 }
 
